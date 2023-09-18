46a66ae36b48cab49278fa59ad4cc4f6ffe8365c
move2slowly <victorphan22@gmail.com>
2023-09-17 23:10:26 -0500

    day 2

---
+++ problem5/solve.py
@@ -0,0 +1,23 @@
+class Solution:
+
+    def longestPalindrome(self, s: str) -> str:
+        pal = ""
+        palMax = 0
+        for i in range(len(s)):
+            left, right = i, i
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if(right - left + 1) > palMax:
+                    pal = s[left:right+1]
+                    palMax = right - left + 1
+                left -=1
+                right +=1
+            
+            left, right = i, i+1
+            while l >= 0 and right < len(s) and s[left] == s[right]:
+                if(right - left + 1) > palMax:
+                    pal = s[left:right+1]
+                    palMax = right - left + 1
+                left -=1
+                right +=1
+        return pal
+        
